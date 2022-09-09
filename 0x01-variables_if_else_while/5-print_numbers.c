262c8e9b444f61d9d98b35a3729383d24c1898df
Osigelialex <osigelialex@gmail.com>
2022-09-09 14:33:15 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char numbers[10];
+	int i;
+
+	numbers = "0123456789";
+
+	for (i = 0; i < 10; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+	
