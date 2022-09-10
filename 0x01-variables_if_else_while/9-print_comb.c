57fc3135787d436ab6a797d6fca79b69b476902e
Osigelialex <osigelialex@gmail.com>
2022-09-09 18:29:02 -0700

    single-digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (number = 0; number <= 9; number++)
 	{
+		putchar((number % 10) + '0');
 		if (number == 9)
 		{
 			continue;
 		}
-		putchar((number % 10) + '0');
 		putchar(',');
 		putchar(' ');
 	}
