7a1bcbe36e76bd4be582daab29669a6409ffd284
Osigelialex <osigelialex@gmail.com>
2022-09-09 18:48:47 -0700

    combination of two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 8; ++i)
+	{
+		for (j = i+1; j <= 9; ++j)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
