8093ae9826a8b699d1a8a84941c016a42a2302db
Osigelialex <osigelialex@gmail.com>
2022-09-13 13:55:37 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * int _abs - function that computes the absolute value of an integer
+ * _abs - function that computes the absolute value of an integer
+ * @n : parameter to find absolute
  * Return: Always 0
  */
 int _abs(int n)
