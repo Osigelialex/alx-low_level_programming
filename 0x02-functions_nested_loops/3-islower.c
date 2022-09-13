5c198bcd2d9bf8a1b883a2f22e327f2c7ff41b1a
Osigelialex <osigelialex@gmail.com>
2022-09-13 13:17:57 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,17 @@
 
 /**
  * _islower -  function that checks for lowercase character
+ * c - this is a parameter
  * Return: 0
  */
 int _islower(int c)
 {
-        if (c >= 97)
-        {
-                return (1);
+	if (c >= 97)
+	{
+		return (1);
 	}
-        else
-        {
-                return (0);
-        }
+	else
+	{
+		return (0);
+	}
 }
