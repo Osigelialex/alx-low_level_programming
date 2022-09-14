abb21463660341aa6a45ba84fde580464df0e457
Osigelialex <osigelialex@gmail.com>
2022-09-14 13:40:55 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,24 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - prints 9 times table starting with 0
  * Return: 0
  */
-void jack_bauer(void)
+void times_table(void)
 {
-	int i, j, k, l;
+	int i, j, value;
 
-	for (i = 0; i <= 2; ++i)
+	for (i = 0; i <= 9; ++i)
 	{
-		for (j = 0; j <= 3; ++j)
+		for (j = 0; j <= 9; ++j)
 		{
-			for (k = 0; k <= 5; ++k)
-			{
-				for (l = 0; l <= 9; ++l)
-				{
-					_putchar((i) + '0');
-					_putchar((j) + '0');
-					_putchar(':');
-					if (l == 9 && i == 2)
-						break;
-					_putchar((k) + '0');
-					_putchar((l) + '0');
-				}
-			}
+			value = j * i;
+			_putchar(value + '0');
+			_putchar(value + ',');
+			if (j == 9 && i == 9)
+				break;
+			_putchar(' ');
 		}
+		_putchar('\n');
 	}
-}
+}			
