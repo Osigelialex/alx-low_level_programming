de014c0e26787c3df11bfc3f86e561393ffb0cc7
Osigelialex <osigelialex@gmail.com>
2022-09-19 12:49:08 -0700

    other characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints other characters
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
