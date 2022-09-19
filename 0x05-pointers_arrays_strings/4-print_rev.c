e0825c9eaeb367dbb178c15379f063c5e62f36cf
Osigelialex <osigelialex@gmail.com>
2022-09-19 11:48:25 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int len, i, j;
+	int count, i, j;
 
-	len = 0;
+	count = 0;
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		len++;
 	}
-	for (j = len; j >= 0; j--)
+	for (j = len-1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
+	_putchar('\n');
 }
