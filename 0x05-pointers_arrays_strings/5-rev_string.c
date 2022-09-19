37c808580b82a3e83a5baac0394d4ad5d26faed6
Osigelialex <osigelialex@gmail.com>
2022-09-19 12:03:32 -0700

    reverse 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,18 @@
  */
 void rev_string(char *s)
 {
-	int i, j;
+	int i, j, k;
 	int count = 0;
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		count++;
 	}
+	for (k = 0; k < count; ++k)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
 	for (j = count - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
