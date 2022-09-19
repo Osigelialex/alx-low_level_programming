74c8586fe63905db8cccbe48afce60d6c176812b
Osigelialex <osigelialex@gmail.com>
2022-09-19 13:19:19 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - puts half
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int count, i, j, k;
+	
+	count = 0;
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (j = count / 2; j < count; ++j)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (k = (count - 1) / 2; k < count; ++k)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+}
