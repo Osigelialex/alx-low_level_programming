e4cb36dcdcbb01e9fd54b177dcbaf5eb1d75f7d0
Osigelialex <osigelialex@gmail.com>
2022-09-19 11:35:22 -0700

    copying

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string to another
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = str;
+	while (*ptr != '\0')
+	{
+		*dest++ = *ptr++;
+	}
+	*(++dest) = '\0';
+	return (dest);
+}
