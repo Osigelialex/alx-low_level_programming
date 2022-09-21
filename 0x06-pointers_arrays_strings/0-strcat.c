a4ee2cce7943798a8d718ac843a103d5a8c17f8f
Osigelialex <osigelialex@gmail.com>
2022-09-21 12:49:34 -0700

    concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	int len1, len2;
+	int len1;
 	char *ptr;
 
 	len1 = 0;
-	len2 = 0;
 	for (i = 0; dest[i] != '\0'; ++i)
 	{
 		len1++;
-	}
-	for (j = 0; src[j] != '\0'; ++j)
-	{
-		len2++;
 	}
 	ptr = dest + len1;
 	while (*src != '\0')
