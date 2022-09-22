8f6ca61098f1ac078a4fddb8bddf85d32561b716
Osigelialex <osigelialex@gmail.com>
2022-09-22 03:59:44 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n characters
+ * @dest: destination
+ * @src: source
+ * @n: n characters
+ * Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len;
+
+	len = 0;
+	for (i = 0; src[i] != '\0' && i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
