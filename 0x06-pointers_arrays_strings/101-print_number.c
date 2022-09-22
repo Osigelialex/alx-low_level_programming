4238aed122325272346c3b04e348787e11ec7999
Osigelialex <osigelialex@gmail.com>
2022-09-22 13:44:14 -0700

    print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: number
+ * Return: void
+ */
+void print_number(int n)
+{
+	char value;
+
+	value = '0' + n;
+	_putchar(value);
+}
