23fe908b6eba8e518471ed4f7de6c5a14923083b
Osigelialex <osigelialex@gmail.com>
2022-09-30 06:04:11 -0700

    printing file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char const *argv[])
 {
-	void(argc);
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
