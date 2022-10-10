5638783fd0d0dd3887fbc3cfb23a5c27b3a60744
Osigelialex <osigelialex@gmail.com>
2022-10-10 13:26:24 -0700

    submit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,13 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 #include <stdio.h>
+
+/**
+ * struct dog - dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 
 struct dog
 {
@@ -6,3 +15,4 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif
