b291501ef298184092f0883de087b29a953ac164
Osigelialex <osigelialex@gmail.com>
2022-10-20 04:55:13 -0700

    printing elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
-
 	while (h != NULL)
 	{
 		if (*h == '\0')
 			printf("[0] (nil)\n");
+		else
+			printf("%s\n", h->str);
 		h = h->next;
-		count++;
 	}
 	return (count);
 }
